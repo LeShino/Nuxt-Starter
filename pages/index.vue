ed33bdd865c539e716b984e34f15869728e8383a
LeShino <laubet@tutanota.de>
2025-08-11 16:00:46 +0000

    Update folder structure

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,15 @@
 </script>
 
 <template>
-  <h1 class="text-6xl text-center underline"> Nuxt 4 + Tailwindcss + Primevue </h1>
+
+  <div class="text-2xl text-center">
+    <h1 class="text-6xl text-center underline"> Nuxt 4 + Tailwindcss + Primevue </h1>
+
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+  </div>
+
 </template>
 
 <style scoped>
